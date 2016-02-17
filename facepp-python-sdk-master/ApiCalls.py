ed59fdba2fd9f52dae1192adec3b8790c78c6be9
Shaham <shahamfarooq@gmail.com>
2016-02-17 11:48:38 -0500

    Fixed #3, tag regex issue
    Still only collecting first 3 tags but now it won't give error when more
    are added.

--- facepp-python-sdk-master/ApiCalls.py
+++ facepp-python-sdk-master/ApiCalls.py
@@ -19,10 +19,12 @@ def get_ads():
     tags = np.zeros((len(jData),4))
     for i in range(0,len(jData)):
         ids.append(jData[i]["_id"])
-        tag_input = re.search('(-*\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*', jData[i]["tags"][0])
-        tags[i,0] = int(tag_input.group(1))
-        tags[i,1] = int(tag_input.group(2))
-        tags[i,2] = int(tag_input.group(3))
+        # tag_input = re.search('(-*\d+)\s*,\s*(\d+)\s*,\s*(\d+).*', jData[i]["tags"][0])
+        t = jData[i]["tags"][0].split(",")
+        tags[i,:] = [int(t[i]) for i in range(0,3)]
+        # tags[i,0] = int(tag_input.group(1))
+        # tags[i,1] = int(tag_input.group(2))
+        # tags[i,2] = int(tag_input.group(3))
         tags[i,3] = i+1
 
     return ids, tags
