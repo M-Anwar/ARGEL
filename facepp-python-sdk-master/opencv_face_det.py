9c5f52fab476eb7db8c15da740fdd236019b06c2
Shaham <shahamfarooq@gmail.com>
2016-02-21 19:07:20 -0500

    Fixed haarcascade compile error.
    put haarcascade directory from openCV into working directory.

--- facepp-python-sdk-master/opencv_face_det.py
+++ facepp-python-sdk-master/opencv_face_det.py
@@ -73,8 +73,8 @@ def divide_im (img,wr,hr,pocx,pocy, faces):
     return divided_imgs
 
 def detect_image (img_url):
-    face_cascade = cv2.CascadeClassifier('C:/Users/Shaham/Downloads/opencv/build/etc/haarcascades/haarcascade_frontalface_alt.xml')
-    eye_cascade = cv2.CascadeClassifier('C:/Users/Shaham/Downloads/opencv/build/etc/haarcascades/haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
     #img_url = 'test_pic/test_face.jpg'
     #img_url = 'test_pic/test_face_fullwide.jpg'
