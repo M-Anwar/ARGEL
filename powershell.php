52082cd4fc76874db09443cf41504ad5d4200332
kimjon12 <davej.kim@mail.utoronto.ca>
2016-03-04 19:56:42 +0000

    moved autopull script to apache server

--- powershell.php
+++
@@ -1,9 +0,0 @@
-<?php
-$psPath = 'c:\\Windows\\System32\WindowsPowerShell\v1.0\\powershell.exe';
-$psDIR = "c:\\wamp\\www\\printer\\scripts\\";
-$psScript = "rabbit.ps1";
-$runCMD = $psPath. ' -ExecutionPolicy RemoteSigned '.$psDIR.$psScript;
-
-exec($psPath, $out);
-echo join($out);
-?>
